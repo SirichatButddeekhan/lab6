49d1e0c5ce25497592ed20e809c19c7fd4c9d80f
sirichat <sirichatcnx60@gmail.com>
2025-12-09 17:08:23 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,29 +2,30 @@
 using namespace std;
 
 int main(){
-    int i=0;
     int n[100];
-    int Even=0;
-    int Odd=0;
-    while(true){
+    int i = 0;
+    int Even = 0;
+    int Odd = 0;
+
+    while (true) {
         cout << "Enter an integer: ";
         cin >> n[i];
 
-        if(n[i] == 0){
-            break;
+        if (n[i] == 0) {
+            break;      
         }
-        if(n[i]%2 == 0){
-            Even += 1;
+
+        if (n[i] % 2 == 0) {
+            Even++;
+        } else {
+            Odd++;
         }
-        else{
-            Odd ++;
-        }
+
         i++;
     }
     
-    cout << "#Even numbers = " << Even;
-    cout << "#Even numbers = " << Odd;
-    
-    
+    cout << "#Even numbers = " << Even << endl;
+    cout << "#Odd numbers = " << Odd;
+
     return 0;
 }
